3634d76a564c36d0de8fc1511ad4ec3198ae589f
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2020-02-18 20:49:48 +0900

    ローマ字->カタカナの変換作成

--- src/Converter/RomaKanaConverter.ts
+++ src/Converter/RomaKanaConverter.ts
@@ -23,13 +23,63 @@ export default class RomaKanaConverter {
         let convertedText: string = '';
         // かなを一文字区切りで分割して，それぞれを変換する
         for (let i = 0; i < text.length; i++) {
-            const convertedChar: string | undefined = this.kana2romaDict.get(text[i]);
-            if (convertedText !== undefined) {
+            const convertedChar: string | undefined = this.kana2romaDict.get(text.charAt(i));
+            if (convertedChar !== undefined) {
                 convertedText += convertedChar;
             } else {
                 throw new Error('変換できない文字を変換しようとしています');
             }
         }
         return convertedText;
+    }
+
+    /**
+     * ローマ字をかなに変換する
+     * @param {string} ローマ字の文字列 ex. 'aiueo'
+     * @returns {string} かなに変換された文字列 ex. 'あいうえお'
+     */
+    public roma2kana(text: string): string {
+        let convertedText: string = '';
+        // textを母音区切りの配列にする
+        const kanaSplitList: string[] = this.createKanaSplitList(text);
+        kanaSplitList.map((roma) => {
+            const convertedChar: string | undefined = this.roma2kanaDict.get(roma);
+            if (convertedChar !== undefined) {
+                convertedText += convertedChar;
+            } else {
+                throw new Error('変換できない文字を変換しようとしています');
+            }
+        });
+        return convertedText;
+    }
+
+    private createKanaSplitList(text: string): string[] {
+        const vowels: string[] = ['a', 'i', 'u', 'e', 'o'];
+        if (text.length === 0) {
+            return [];
+        }
+        // まず文字の区切れ目のidxを探す
+        let kanaCharIdx: number[] = [0];
+        for (let i = 0; i < text.length; i++) {
+            // 切れ目の基準: 
+            // 母音->次の文字から新しい区切り
+            if (i + 1 < text.length && vowels.includes(text.charAt(i))) {
+                kanaCharIdx.push(i + 1);
+            // nが2連続 -> 1つ目のnからみて2文字あとから新しい区切り
+            // TODO: nn nnの時は対応できないので追加
+            } else if (i + 2 < text.length && i - 1 > 0 && vowels.includes(text.charAt(i-1)) && text.charAt(i) === 'n' && text.charAt(i + 1) === 'n') {
+                kanaCharIdx.push(i + 2);
+            }
+        }
+        kanaCharIdx = Array.from(new Set(kanaCharIdx)); // 重複を削除
+        const kanaSplitList: string[] = [];
+        for (let i = 0; i < kanaCharIdx.length; i++) {
+            if (i !== kanaCharIdx.length - 1) {
+                kanaSplitList.push(text.slice(kanaCharIdx[i], kanaCharIdx[i + 1]));
+            } else {
+                kanaSplitList.push(text.slice(kanaCharIdx[i]));
+            }
+        }
+        return kanaSplitList;
     }
 }
--- tests/unit/Converter/romaKanaConverter.spec.ts
+++ tests/unit/Converter/romaKanaConverter.spec.ts
@@ -2,21 +2,27 @@ import RomaKanaConverter from '@/Converter/RomaKanaConverter';
 
 describe('ローマ字，かなの変換', () => {
     const converter = new RomaKanaConverter();
+    const rules = new Map<string, string>([
+        ['アイウエオ', 'aiueo'],
+        ['ア', 'a'],
+        ['モ', 'mo'],
+        ['モライモライ', 'moraimorai'],
+        ['', ''],
+    ]);
 
     it('かな->ローマ字', () => {
-        const rules = new Map<string, string>([
-            ['ア', 'a'],
-            ['アイウエオ', 'aiueo'],
-            ['モ', 'mo'],
-            ['モライモライ', 'moraimorai'],
-            ['', ''],
-        ]);
         rules.forEach((roma, kana) => {
             expect(converter.kana2roma(kana)).toBe(roma);
         })
     });
 
-    it('ローマ字->かな', () => {
+    it('ローマ字をかな基準で分割する', () => {
+        expect((converter as any).createKanaSplitList('konnnitiha')).toStrictEqual(['ko', 'nn', 'ni', 'ti', 'ha'])
+    });
 
+    it('ローマ字->かな', () => {
+        rules.forEach((roma, kana) => {
+            expect(converter.roma2kana(roma)).toBe(kana);
+        })
     });
 });
