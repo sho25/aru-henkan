068f1e1d65bd849bfb153eafbb5376ca2f5023a8
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2020-02-19 11:34:01 +0900

    vuetifyに対応

--- src/components/Main/index.vue
+++ src/components/Main/index.vue
@@ -1,17 +1,34 @@
 <template>
     <v-app id='main'>
-        <h1>ある変換</h1>
-        <div v-if="!buildSuccess">
-            {{ initialMessage }}
-        </div>
-        <div v-else>
-            <!-- <MsgDisplayer :message="aruGyakuHenkan"/> -->
-            <!-- <p>↑ある逆変換</p> -->
-            <v-text-field v-model="userInput"/>
-            <p>↓ある変換</p>
-            <MsgDisplayer :message="aruHenkan"/>
-        </div>
+        <v-container>
+            <h1>ある変換</h1>
+            <div v-if="!buildSuccess">
+                {{ initialMessage }}
+            </div>
+            <div v-else>
+                <!-- <MsgDisplayer :tokenizedMessage="aruGyakuHenkan"/> -->
+                <!-- <p>↑ある逆変換</p> -->
+                <v-text-field 
+                    class="user-input"
+                    v-model="userInput"
+                    clearable
+                    Solo
+                    outlined
+                />
+                <p>↓ある変換</p>
+                <MsgDisplayer :tokenizedMessage="aruHenkan"/>
+            </div>
+        </v-container>
     </v-app>
 </template>
 
-<script lang='ts' src='./index.ts'>
+<script lang='ts' src='./index.ts'/>
+
+<style scoped>
+.v-text-field {
+    width: 50%;
+}
+.v-input__slot {
+    align-items: center;
+}
+</style>
